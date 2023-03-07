2aeee54deef9e9f6dfd7364b33e2773a62cccca7
RobWar-code <rowaniar@aol.com>
2023-03-07 16:56:21 +0000

    Initial example

---
+++ evens.py
@@ -0,0 +1,13 @@
+def even_number_of_evens(numbers):
+    """
+        Return an error if the type of numbers is not a list
+        If no numbers return False
+        If even number of evens return True
+        If odd number of evens return False
+        If no even numbers return False
+    """
+    return None
+
+
+if __name__ == "__main__":
+    print(even_number_of_evens(5))
